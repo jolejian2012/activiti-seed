7a2a64b2e8eaefe35a4f2712d8c3b3f7874c2267
winily <18934086807@163.com>
2020-04-05 22:07:23 +0800

    face: NO：2020-0x01

---
+++ src/main/java/com/liwnily/activiti/seed/ActivitiSeedApplication.java
@@ -0,0 +1,16 @@
+package com.liwnily.activiti.seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(
+        exclude = {
+                org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+                org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+        }
+)
+public class ActivitiSeedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiSeedApplication.class, args);
+    }
+}
